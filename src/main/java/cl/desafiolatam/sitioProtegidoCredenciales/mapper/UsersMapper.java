8ba187176e0f38562d20355577d62123698afdb1
Carolina Diaz <cdiaza87@gmail.com>
2021-11-24 12:02:12 -0300

    Segundo Commit solucionado error bean, otro error bean

--- src/main/java/cl/desafiolatam/sitioProtegidoCredenciales/mapper/UsersMapper.java
+++ src/main/java/cl/desafiolatam/sitioProtegidoCredenciales/mapper/UsersMapper.java
@@ -1,5 +1,12 @@
 package cl.desafiolatam.sitioProtegidoCredenciales.mapper;
 
-public interface UsersMapper {
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
+import cl.desafiolatam.sitioProtegidoCredenciales.model.Users;
+
+public interface UsersMapper {
+	
+	@Select("select * from users where email = #{email}")
+	Users findByEmail(@Param("email") String email);
 }
